bd72574b5b5a5dadbcc5eaed92fd8ee117818236
prajapati123476 <prajapativishwa671@gmail.com>
2021-10-01 01:16:44 +0530

    database

--- js/script.js
+++ js/script.js
@@ -94,36 +94,3 @@ function saveData(name, email, phone, message) {
 
 
 
-
-
-
-
-
-
-
-
-
-// <script type="module">
-//   // Import the functions you need from the SDKs you need
-//   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
-//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-analytics.js";
-//   // TODO: Add SDKs for Firebase products that you want to use
-//   // https://firebase.google.com/docs/web/setup#available-libraries
-
-//   // Your web app's Firebase configuration
-//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//   const firebaseConfig = {
-//     apiKey: "AIzaSyCmI7JOXmx6KONjXYRwTurEOcsW9wHY-Rs",
-//     authDomain: "contactform-662da.firebaseapp.com",
-//     projectId: "contactform-662da",
-//     storageBucket: "contactform-662da.appspot.com",
-//     messagingSenderId: "890060447067",
-//     appId: "1:890060447067:web:bc9921267e57b06c624e7e",
-//     measurementId: "G-E2YWW6L0X5"
-//   };
-
-//   // Initialize Firebase
-//   const app = initializeApp(firebaseConfig);
-//   const analytics = getAnalytics(app);
-// </script>
-
